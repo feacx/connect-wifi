24cad974627fc5940dc5c025f221a5daf20ee342
Fea <fea.x.cn@gmail.com>
2021-12-01 00:26:50 +0800

    -

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{title}}</text>
+			{{ JSON.stringify(wifiList) }}
 		</view>
 	</view>
 </template>
@@ -11,19 +10,86 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				wifiList: null
 			}
 		},
 		onLoad() {
-
+			this.startWifi();
+			// wx.scanCode({
+			// 	success: (res) => {
+			// 		console.log("扫码结果");
+			// 		console.log(res);
+			// 	},
+			// 	fail: (res) => {
+			// 		console.log(res);
+			// 	}
+			// })
 		},
 		methods: {
+			startWifi() {
+				wx.showLoading({
+					title: '正在搜索可连接wifi',
+				})
 
+				wx.startWifi({
+					success: (res) => {
+						console.log('startWifi', res)
+						wx.getWifiList({
+							success: () => {
+								wx.onGetWifiList(res => {
+									this.wifiList = res;
+									wx.hideLoading({});
+									console.log('getWifiList', res)
+									console.log(res)
+									if(res.wifiList.length !== 0) {
+										// 可以对获取的wifi列表进行数据处理或者筛选
+										// 根据项目要求展示wifi列表界面
+									}else{
+										wx.showToast({
+											title: '暂无可连接wifi,请重试',
+											icon: 'none'
+										})
+									}
+								})
+							},  
+							fail: (e) => {
+								console.log('获取wifi列表失败', e)
+								wx.hideLoading({})
+							}
+						})
+					},
+					fail: (e) => {
+						console.log('wifi连接失败', e)
+						wx.hideLoading({})
+					},
+				})
+			},
+			
+			// 选择wifi连接
+			onChangeWifi(e) {
+				let that = this
+				let item = e.detail.data
+				wx.connectWifi({
+					SSID: item.SSID,
+					BSSID: item.BSSID,
+					password: '',
+					success: function (res) {
+						console.log('connectWifi suc', res)
+					},
+					fail: function (e) {
+						console.log('connectWifi fail',e)
+						wx.showToast({
+							title: '设备wifi连接失败',
+							icon: 'none'
+						})
+					}
+				})
+			},
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
